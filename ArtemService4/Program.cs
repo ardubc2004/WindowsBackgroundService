79ba867e0cb71c76146dfa4fef52272108d34d69
Artem Dubchenko <ardubc@ttu.ee>
2025-10-29 13:29:02 +0200

    Update readme.md, minor changes to logger information levels

--- ArtemService4/Program.cs
+++ ArtemService4/Program.cs
@@ -8,7 +8,7 @@ Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Information()
     .WriteTo.Console()
     .WriteTo.File(
-        path: "Logs/log-.txt",
+        path: @"C:\Users\dubch\Documents\VS Publish Dir\MyService\Logs\log-.txt",
         rollingInterval: RollingInterval.Minute,
         retainedFileCountLimit: 5,
         outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
@@ -26,7 +26,29 @@ try
     })
     .Build();
 
+    try
+    {
+        if (args == null)
+        {
+            Log.Information("args is null");
+        }
+        else if (args.Length == 0)
+        {
+            Log.Information("args is empty");
+        }
+        else
+        {
+            Log.Information(String.Join("||", args));
+        }
+
+
+    }
+    catch (Exception ex)
+    {
+        Log.Error($"Exception {ex}");
+    }
     await host.RunAsync();
+
 }
 catch (Exception ex)
 {
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 To create the service (in cmd):
-sc create "ArtemService" binPath="(path to the service executable)" --k --t
+sc create "ArtemService" binPath="(path to the service executable) --k --t"
+example:
+sc create "ArtemService" binPath="C:\Users\dubch\Documents\VS Publish Dir\MyService\ArtemService4.exe --k --t"
